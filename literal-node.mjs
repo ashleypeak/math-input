be04d347f1ddea3ea307d69685d895f5c1f72476
Wyatt Peak <wyattpeak@gmail.com>
2019-04-16 16:00:28 +1000

    Commented code

--- literal-node.mjs
+++ literal-node.mjs
@@ -1,20 +1,59 @@
+/**
+ * Virtual class, don't instantiate.
+ *
+ * LiteralNode and its children represent the equation as it appears in the
+ * input field with only enough structure to render (e.g. it has a Division
+ * node to render numerator/denominator, but doesn't distinguish between other
+ * operators).
+ *
+ * Convert to a SemanticNode for structure.
+ */
 class LiteralNode {
+    /**
+     * @constructs
+     */
     constructor() {
         this._parent = null;
     }
 
+
+    /** GETTERS AND SETTERS */
+
+    /**
+     * Set parent node
+     * 
+     * @param  {LiteralNode} parent The new parent node of this element
+     */
     set parent(parent) {
         this._parent = parent;
     }
 
+    /**
+     * Get the value - the string which will go into the value attribute.
+     * In future will be determined by output type.
+     *
+     * @abstract
+     */
     get value() {
         throw new Error('get value() must be defined.');
     }
 
+    /**
+     * Get the DOM node of the LiteralNode. LiteralNodes maintain their own
+     * DOM elements
+     */
     get element() {
-        throw new Error('get element() must be defined.');
+        return this._element;
     }
+    
 
+    /** MISCELLANEOUS FUNCTIONS */
+
+    /**
+     * Add a class to the LiteralNode's DOM element, if not present
+     * 
+     * @param {string} className The class to be added
+     */
     addClass(className) {
         var classes = this._element.className.split(' ');
 
@@ -26,6 +65,11 @@ class LiteralNode {
         this._element.className = classes.join(' ');
     }
 
+    /**
+     * Remove a class from the LiteralNode's DOM element, if present
+     * 
+     * @param {string} className The class to be removed
+     */
     removeClass(className) {
         var classes = this._element.className.split(' ');
 
@@ -37,6 +81,12 @@ class LiteralNode {
         this._element.className = classes.join(' ');
     }
 
+    /**
+     * Toggle a class on or off in the LiteralNode's DOM element,
+     * depending on current state
+     * 
+     * @param {string} className The class to be toggled
+     */
     toggleClass(className) {
         var classes = this._element.className.split(' ');
 
@@ -50,6 +100,11 @@ class LiteralNode {
         this._element.className = classes.join(' ');
     }
 
+    /**
+     * Toggle the cursor class. Used by MathInput.constuctor to flash cursor.
+     * 
+     * @param  {String} state (on|off|toggle) Override toggle to show/hide cursor
+     */
     toggleCursor(state='toggle') {
         if(state == 'toggle') {
             this.toggleClass('cursor');
@@ -60,47 +115,88 @@ class LiteralNode {
         }
     }
 
+
+    /** EXPORTED STATIC FUNCTIONS */
+
+    /**
+     * Take a character (from input, usually) and determine based on value
+     * what LiteralNode class to return.
+     * 
+     * @param  {String} char The character to create a LiteralNode from
+     * @return {LiteralNode} The resultant LiteralNode
+     */
     static buildFromCharacter(char) {
         return new LiteralCharacterNode(char);
     }
 }
 
+/**
+ * LiteralExpressionNode contains any string of nodes which doesn't need to
+ * be distinguished for display purposes. Divisions, Exponents etc will have
+ * other LiteralNode types, but anything else will be stored in the array
+ * this._nodes.
+ *
+ * The numerator and denominator of a division, exponent of a power etc. are
+ * all ExpressionNodes, as is the root node of the MathInput.
+ */
 class LiteralExpressionNode extends LiteralNode {
+    /**
+     * @constructs
+     */
     constructor() {
         super();
 
         this._nodes = new Array();
         this._element = document.createElement('span');
-        this._element.setAttribute('class', 'empty-expression');
 
         //every expression starts with a span, serves as the cursor location before
         //the first element
         this.insert(new LiteralExpressionStartNode());
     }
 
+
+    /** GETTERS AND SETTERS */
+
+    /**
+     * Get the value - the string which will go into the value attribute.
+     */
     get value() {
         return this._nodes.reduce((acc, node) => acc + node.value, '');
     }
 
-    get element() {
-        return this._element;
-    }
-
+    /**
+     * Get the array of child nodes
+     */
     get nodes() {
         return this._nodes;
     }
 
+
+    /** MISCELLANEOUS */
+
+    /**
+     * Insert a node into this expression. Currently inserts at the end, but
+     * will ultimately be determined by cursor position
+     * 
+     * @param  {LiteralNode} node The node to be inserted
+     */
     insert(node) {
         node.parent = this;
         this._nodes.push(node);
         this._element.appendChild(node.element);
-
-        if(this.nodes.length > 1) //1 == empty because of the LiteralExpresionStartNode
-            this.removeClass('empty-expression');
     }
 }
 
+/**
+ * A LiteralExpressionStartNode is present as the first node of every
+ * LiteralExpressionNode and nowhere else. Its purpose is to display the cursor
+ * if it is left of every other element in the LiteralExpressionNode, and to
+ * provide an anchor to click on / interact with an empty expression.
+ */
 class LiteralExpressionStartNode extends LiteralNode {
+    /**
+     * @constructs
+     */
     constructor() {
         super();
 
@@ -108,16 +204,18 @@ class LiteralExpressionStartNode extends LiteralNode {
         this._element.className = 'empty';
     }
 
+    /**
+     * Get the value - the string which will go into the value attribute.
+     */
     get value() {
         return '';
-    }
-
-    get element() {
-        return this._element;
     }
 }
 
 class LiteralCharacterNode extends LiteralNode {
+    /**
+     * @constructs
+     */
     constructor(char) {
         super();
         this._char = char;
@@ -126,12 +224,12 @@ class LiteralCharacterNode extends LiteralNode {
         this._element.innerText = this._char;
     }
 
+
+    /**
+     * Get the value - the string which will go into the value attribute.
+     */
     get value() {
         return this._char;
-    }
-
-    get element() {
-        return this._element;
     }
 }
 
--- math-input.mjs
+++ math-input.mjs
@@ -15,6 +15,8 @@ template.innerHTML = `
             white-space: nowrap;
             overflow: hidden;
             cursor: text;
+            font-family: "Helvetica Neue", Arial, sans-serif;
+            font-size: 12pt;
         }
 
         .wrapper span {
@@ -22,7 +24,7 @@ template.innerHTML = `
             vertical-align: middle;
             padding: 0px 1px 0px 0px;
             cursor: text;
-            min-width: 7px;
+            min-width: 5px;
             min-height: 20px;
         }
 
@@ -30,22 +32,22 @@ template.innerHTML = `
             padding-right: 0px;
             border-right: 1px solid;
         }
-
-        .wrapper .empty-expression {
-            background-color: #d9edf7;
-            border: 1px solid #31708f;
-        }
-
-        .mathinput .empty-expression.cursor-inside {
-            background-color: #dff0d8;
-            border: 1px solid #3c763d;
-        }
     </style>
     <div id='wrapper' class='wrapper'>
     </div>
 `
 
+/**
+ * The MathInput is a form element which allows for the entry of mathematical
+ * expressions.
+ *
+ * NOTE: Due to limitations in creating WebComponent form elements, MathInput
+ * fields must be given a tabindex.
+ */
 class MathInput extends HTMLElement {
+    /**
+     * @constructs
+     */
     constructor() {
         super();
 
@@ -53,11 +55,12 @@ class MathInput extends HTMLElement {
         this.shadowRoot.appendChild(template.content.cloneNode(true));
         this.wrapper = this.shadowRoot.getElementById('wrapper');
 
-        this.literalTree = new LiteralExpressionNode(null);
-        this.expression = this.literalTree;
+        this.rootNode = new LiteralExpressionNode(null);
+        this.expression = this.rootNode;
         this._cursorNode = this.expression.nodes[0];
 
-        this.render();
+        // this.render();
+        this.wrapper.appendChild(this.rootNode.element);
 
         var self = this;
         setInterval(function() {
@@ -71,6 +74,14 @@ class MathInput extends HTMLElement {
         this.addEventListener('blur', this.blur);
     }
 
+
+    /** WEBCOMPONENTS CONTROL */
+
+    /** 
+    * Attach the input which serves as the form element. Input element can't be
+    * in the shadow DOM, and you can't attach elements to the main DOM until
+    * after connection.
+    */
     connectedCallback() {
         //proxy input
         var input = document.createElement('input');
@@ -83,17 +94,38 @@ class MathInput extends HTMLElement {
         this._connected = true;
     }
 
+    /**
+     * Updates the input element whenever the value is changed (by render())
+     * 
+     * @param  {string} name  The name of the changed attribute
+     * @param  {string} old   The old value of the attribute
+     * @param  {string} value The new attribute value
+     */
     attributeChangedCallback(name, old, value) {
         if(this._connected) {
-            this.shadowRoot.getElementById('wrapper').innerHTML = value;
             this.getElementsByClassName('real-input')[0].value = value;
         }
     }
 
+    /**
+     * Returns the list of attributes which, when changed, should trigger
+     * attributeChangedCallback
+     * 
+     * @return {array} The list of attributes to watch
+     */
     static get observedAttributes() {
         return ['value'];
     }
 
+
+    /** EVENT HANDLERS */
+
+    /**
+     * OnKeyDown, determine if the key should be intercepted and, if so,
+     * insert/delete node, move cursor etc.
+     * 
+     * @param  {Event} e JavaScript Event object
+     */
     keydown(e) {
         if(e.ctrlKey)   //don't capture control combinations
             return;
@@ -105,40 +137,65 @@ class MathInput extends HTMLElement {
         }
     }
 
+    /**
+     * OnFocus, start the cursor flashing
+     */
     focus() {
         this._focused = true;
         this.cursorNode.toggleCursor('on');
     }
 
+    /**
+     * OnBlur, stop the cursor flashing
+     */
     blur() {
         this._focused = false;
         this.cursorNode.toggleCursor('off');
     }
 
+
+    /** GETTERS AND SETTERS */
+
+    /**
+     * Get cursorNode - the node with the flashing cursor and after which new
+     * nodes will be inserted
+     */
     get cursorNode() {
         return this._cursorNode;
     }
 
+    /**
+     * Set cursorNode. Remove the cursor class from the previous cursor node
+     * before changing.
+     * 
+     * @param  {LiteralNode} node The new cursorNode
+     */
     set cursorNode(node) {
         this._cursorNode.toggleCursor('off');
         this._cursorNode = node;
     }
 
+
+    /** MISCELLANEOUS (CHANGE WHEN THERE'S MORE STRUCTURE) */
+
+    /**
+     * Insert a character at the cursor position.
+     * 
+     * @param  {string} char The character to insert as a new node
+     */
     insert(char) {
         var node = LiteralNode.buildFromCharacter(char);
         this.expression.insert(node);
         this.cursorNode = node;
 
-        this.render();
+        this.updateValue();
     }
 
-    render() {
-        this.setAttribute('value', this.literalTree.value);
-
-        while(this.wrapper.firstChild) {
-            this.wrapper.removeChild(this.wrapper.firstChild);
-        }
-        this.wrapper.appendChild(this.literalTree.element);
+    /**
+     * Update the element's value to reflect the expression in rootNode
+     */
+    updateValue() {
+        this.setAttribute('value', this.rootNode.value);
     }
 }
 
